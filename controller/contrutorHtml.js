a9e0dce4ef7978c7442a619602460487abbf0314
Rafa Souza <rlcitriny@gmail.com>
2021-08-17 01:51:00 -0300

    Criação html parra envio http

--- controller/contrutorHtml.js
+++ controller/contrutorHtml.js
@@ -2,20 +2,47 @@ const DOMParser = require("dom-parser");
 const parser = new DOMParser();
 const fs = require("fs");
 const pastalocal = "controller/";
+const Dbase = require("../database/database.js");
+const dbase = new Dbase();
+class createhtml {
 
-const newhtml = (arquivo) => {
-  if (!fs.existsSync(pastalocal + arquivo)) return "Arquivo não existe.";
-  const htmlopen = fs.readFileSync(pastalocal + arquivo, "utf-8");
-  const html = parser.parseFromString(htmlopen, "text/html");
-  
-  html.getElementsByClassName("div")[0].innerHTML = "<p>Rafa</p>";
+   newhtml = (arquivo) => {
+    if (!fs.existsSync(pastalocal + arquivo)) return "Arquivo não existe.";
+    const htmlopen = fs.readFileSync(pastalocal + arquivo, "utf-8");
+    let htmlBurguer = htmlopen.split('#&&#')
+    let html = htmlBurguer[0].toString(); //#&&#
+     
+    for (const filme of dbase.buscar("Filmes.json",'harry')) {
+      html +=  '<div class="card">' +
+        '<img src="' +
+        filme.imgULR +
+        '" alt="Avatar" ></img>' +
+        '<div class="container">' +
+        "<h4><b>" +
+        filme.nome +
+        "</b></h4> " +
+        "<p>" +
+        filme.ano +
+        "</p>" +
+        " <button>Detalhe</button> " +
+        " </div>" +
+        "</div>"
+    }
+    html += htmlBurguer[1].toString();
+    console.log(html)
+    
+    return html;
+  };
+}
 
-  fs.writeFileSync(`${pastalocal}${arquivo}`, html);
-};
-newhtml("new.html");
+
+
 
 //const data = fs.readFileSync(`${pastalocal}${arquivo}`, "utf-8");
 //const objetox = JSON.parse(data.toString());
 //objetox.push(objeto);
 //const objetoJson = JSON.stringify(objetox);
 //fs.writeFileSync(`${pastalocal}${arquivo}`, objetoJson);
+
+
+module.exports = createhtml;
